bd6379266c81dec2249d5e50ae549b1808fbc7f1
Li-Xian Chen <b0543017@ems.niu.edu.tw>
2019-06-16 21:06:57 +0800

    finish controll function

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'LXC' });
+});
+
+router.get('/control/:id', function(req, res, next) {
+  res.render('control', { title: 'RMC', id: req.params.id });
 });
 
 module.exports = router;
